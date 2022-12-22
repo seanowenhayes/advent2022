5fe606839272f7a04b05a25392a83772016e8a1e
sean <seanowenhayes@gmail.com>
2022-12-22 23:53:28 +0100

    Do day 5 part 1

--- 05/index.ts
+++ 05/index.ts
@@ -4,8 +4,30 @@ export type Input = {
   instructions: Instruction[];
 };
 
+export function isACrateLine(input: string) {
+  return input.indexOf("[") !== -1;
+}
+
+export function isAnInstructionLine(line: string) {
+  return line[0] === "m";
+}
 export function parseInput(input: string): Input {
-  return { crates: [], instructions: [] };
+  const lines = input.split("\n");
+  const crateLines = [];
+  const instructions = [];
+  for (const line of lines) {
+    if (isACrateLine(line)) {
+      console.log({ line });
+      const crateLine = parseCrateLine(line);
+      crateLines.push(crateLine);
+    }
+    if (isAnInstructionLine(line)) {
+      const instruction = parseInstruction(line);
+      instructions.push(instruction);
+    }
+  }
+  const crates = crateFromLines(crateLines.reverse());
+  return { crates, instructions };
 }
 
 export function parseCrateLine(input: string) {
@@ -23,11 +45,44 @@ export function crateFromLines(lines: Array<Array<string>>) {
       columns[index] = column + item;
     });
     return columns;
-  }, []);
+  }, []).map((column) => column.trim());
 }
 
-export function parseInstruction(instruction: string) {}
+const instructionRegex = /[^\d]+(\d+)[^\d]+(\d+)[^\d]+(\d+)/;
+export function parseInstruction(instruction: string) {
+  const result = instruction.match(instructionRegex);
+  if (result) {
+    const [_, quantity, from, to] = result;
+    return { quantity: +quantity, from: +from, to: +to };
+  }
+  return { quantity: 0, from: 0, to: 0 };
+}
 
-export function parseCrates(input: string) {
-  input.split("\n");
+export function makeMove(
+  crates: Input["crates"],
+  { from, quantity, to }: Instruction,
+) {
+  const fromIndex = from - 1;
+  const toIndex = to - 1;
+  const fromColumn = crates[fromIndex];
+  const toMove = fromColumn.substr(-quantity).split("").reverse().join("");
+  const fromColumnAfterMove = fromColumn.substr(
+    0,
+    fromColumn.length - quantity,
+  );
+  const toColumn = crates[toIndex];
+  const toColumnAfterMove = toColumn + toMove;
+  crates[fromIndex] = fromColumnAfterMove;
+  crates[toIndex] = toColumnAfterMove;
+  return crates;
+}
+
+export function doIt(input: Input) {
+  const { crates, instructions } = input;
+  const finalCrates = instructions.reduce(
+    (crates, instruction) => makeMove(crates, instruction),
+    crates,
+  );
+
+  return finalCrates;
 }
--- 05/test.ts
+++ 05/test.ts
@@ -1,5 +1,14 @@
+import { input, sampleInput } from "./input.ts";
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { crateFromLines, parseCrateLine, parseCrates } from "./index.ts";
+import {
+  crateFromLines,
+  doIt,
+  Input,
+  isACrateLine,
+  parseCrateLine,
+  parseInput,
+  parseInstruction,
+} from "./index.ts";
 
 Deno.test(function parseCrateLineOneLetter() {
   const input = `[D]     `;
@@ -37,5 +46,116 @@ Deno.test(function crateWithTwoLines() {
     ["M", "M", "M", "M", "M", "M", "M", "M", "M"],
   ];
   const output = crateFromLines(input);
-  assertEquals(["VM", " M", "BM", " M", " M", " M", " M", " M", "CM"], output);
+  assertEquals(["VM", "M", "BM", "M", "M", "M", "M", "M", "CM"], output);
+});
+
+Deno.test(function testParseInstruction() {
+  const input = `move 1 from 2 to 1`;
+  const instruction = parseInstruction(input);
+  assertEquals(instruction, { quantity: 1, from: 2, to: 1 });
+});
+
+Deno.test(function testParseInstructionLargerNumbers() {
+  const input = `move 11 from 21 to 221`;
+  const instruction = parseInstruction(input);
+  assertEquals(instruction, { quantity: 11, from: 21, to: 221 });
+});
+
+Deno.test(function makeMoveFromcolumnOneToColumnTwo() {
+  const input: Input = {
+    crates: ["A", "B", "C"],
+    instructions: [{ quantity: 1, from: 1, to: 2 }],
+  };
+  const crate = doIt(input);
+  assertEquals(crate, ["", "BA", "C"]);
+});
+
+Deno.test(function makeMoveFromcolumnTwoToColumnOne() {
+  const input: Input = {
+    crates: ["AZ", "BX", "CY"],
+    instructions: [{ quantity: 1, from: 2, to: 1 }],
+  };
+  const crate = doIt(input);
+  assertEquals(crate, ["AZX", "B", "CY"]);
+});
+
+Deno.test(function sampleFirstMove() {
+  // const {crates, instructions} = parseInput(sampleInput)
+  const crates = ["ZN", "MCD", "P"];
+  const firstInstruction = { quantity: 1, from: 2, to: 1 };
+  const crate = doIt({ crates, instructions: [firstInstruction] });
+  assertEquals(crate, ["ZND", "MC", "P"]);
+});
+
+Deno.test(function sampleSecondMove() {
+  // const {crates, instructions} = parseInput(sampleInput)
+  const crates = ["ZND", "MC", "P"];
+  const firstInstruction = { quantity: 3, from: 1, to: 3 };
+  const crate = doIt({ crates, instructions: [firstInstruction] });
+  assertEquals(crate, ["", "MC", "PDNZ"]);
+});
+
+Deno.test(function sampleThirdMove() {
+  const crates = ["", "MC", "PDNZ"];
+  const firstInstruction = { quantity: 2, from: 2, to: 1 };
+  const crate = doIt({ crates, instructions: [firstInstruction] });
+  assertEquals(crate, ["CM", "", "PDNZ"]);
+});
+
+Deno.test(function sampleFourthMove() {
+  const crates = ["CM", "", "PDNZ"];
+  const firstInstruction = { quantity: 1, from: 1, to: 2 };
+  const crate = doIt({ crates, instructions: [firstInstruction] });
+  assertEquals(crate, ["C", "M", "PDNZ"]);
+});
+
+Deno.test(function isACrateLineIdentified() {
+  const crateLine = "    [D]        ";
+  const isCrateLine = isACrateLine(crateLine);
+  assertEquals(isCrateLine, true);
+});
+
+Deno.test(function emptyLineIsNotACrateline() {
+  const crateLine = "";
+  const isCrateLine = isACrateLine(crateLine);
+  assertEquals(isCrateLine, false);
+});
+
+Deno.test(function parseSampleInputCratesCorrectly() {
+  const input = parseInput(sampleInput);
+  assertEquals(input.crates, ["ZN", "MCD", "P"]);
+});
+
+Deno.test(function parseSampleInputInstructionCorrectly() {
+  const input = parseInput(sampleInput);
+  assertEquals(input.instructions, [
+    { quantity: 1, from: 2, to: 1 },
+    { quantity: 3, from: 1, to: 3 },
+    { quantity: 2, from: 2, to: 1 },
+    { quantity: 1, from: 1, to: 2 },
+  ]);
+});
+
+Deno.test(function parseSampleInput() {
+  const crates = parseInput(sampleInput);
+  assertEquals(crates, {
+    crates: ["ZN", "MCD", "P"],
+    instructions: [
+      { quantity: 1, from: 2, to: 1 },
+      { quantity: 3, from: 1, to: 3 },
+      { quantity: 2, from: 2, to: 1 },
+      { quantity: 1, from: 1, to: 2 },
+    ],
+  });
+});
+
+Deno.test(function doesTheSampleWork() {
+  const finalCrates = doIt(parseInput(sampleInput));
+  assertEquals(finalCrates, ["C", "M", "PDNZ"]);
+});
+
+Deno.test(function reallyDoIt() {
+  const finalCrates = doIt(parseInput(input));
+  console.log({ finalCrates });
+  assertEquals(true, true);
 });
